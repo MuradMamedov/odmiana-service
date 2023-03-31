e5b5fd51f616c0152b59868f056a5b4fcb5f647b
Murad Mamedov <murad.mamedov@relativity.com>
2023-03-31 15:06:50 +0200

    MAIN added middleware

--- handlers/products.go
+++ handlers/products.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"micro-service/data"
 	"net/http"
@@ -26,16 +27,10 @@ func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
+func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 
-	if err != nil {
-		p.l.Printf("Unable to deserialize products. Error: %#v", err)
-		http.Error(rw, "Unable to deserialize products", http.StatusBadRequest)
-	}
-
-	data.AddProduct(prod)
+	data.AddProduct(&prod)
 }
 
 func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
@@ -45,16 +40,9 @@ func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Unable to convert ID", http.StatusBadRequest)
 	}
 	p.l.Println("Handle PUT Product ", id)
+	prod := r.Context().Value(KeyProduct{}).(data.Product)
 
-	prod := &data.Product{}
-	err = prod.FromJSON(r.Body)
-
-	if err != nil {
-		p.l.Printf("Unable to deserialize products. Error: %#v", err)
-		http.Error(rw, "Unable to deserialize products", http.StatusBadRequest)
-	}
-
-	err = data.UpdateProduct(id, prod)
+	err = data.UpdateProduct(id, &prod)
 	if err == data.ErrProductNotFound {
 		http.Error(rw, "Product not found", http.StatusNotFound)
 		return
@@ -63,4 +51,24 @@ func (p *Products) UpdateProducts(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Product not found", http.StatusInternalServerError)
 		return
 	}
+}
+
+type KeyProduct struct{}
+
+func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := &data.Product{}
+
+		err := prod.FromJSON(r.Body)
+
+		if err != nil {
+			p.l.Printf("Unable to deserialize products. Error: %#v", err)
+			http.Error(rw, "Unable to deserialize products", http.StatusBadRequest)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), KeyProduct{}, *prod)
+		req := r.WithContext(ctx)
+		next.ServeHTTP(rw, req)
+	})
 }
--- main.go
+++ main.go
@@ -17,11 +17,16 @@ func main() {
 	ph := handlers.NewProducts(l)
 
 	sm := mux.NewRouter()
-	getRouter := sm.Methods("GET").Subrouter()
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
 	getRouter.HandleFunc("/", ph.GetProducts)
 
-	putRouter := sm.Methods("PUT").Subrouter()
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
 	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareProductValidation)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareProductValidation)
 
 	s := &http.Server{
 		Addr:         ":9090",
