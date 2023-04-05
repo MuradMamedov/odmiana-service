873d2962a30915607fc0d3253f4b2bac202c4de6
Murad Mamedov <murad.mamedov@relativity.com>
2023-04-05 15:44:15 +0200

    MAIN added validation

--- handlers/products.go
+++ handlers/products.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"micro-service/data"
 	"net/http"
@@ -62,11 +63,20 @@ func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
 		err := prod.FromJSON(r.Body)
 
 		if err != nil {
-			p.l.Printf("Unable to deserialize products. Error: %#v", err)
+			p.l.Println("Unable to deserialize products. Error", err)
 			http.Error(rw, "Unable to deserialize products", http.StatusBadRequest)
 			return
 		}
 
+		//validate the product
+		err = prod.Validate()
+		if err != nil {
+			p.l.Println("[ERROR] validating product", err)
+			http.Error(rw, fmt.Sprintf("Error validating product: %s", err), http.StatusBadRequest)
+			return
+		}
+
+		// add the product to the context
 		ctx := context.WithValue(r.Context(), KeyProduct{}, *prod)
 		req := r.WithContext(ctx)
 		next.ServeHTTP(rw, req)
